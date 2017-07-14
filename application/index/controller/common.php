b0e0377a8090e823fa9a68bf77e5e2fa1c87dc3d
15195044405 <1262660796@qq.com>
2017-07-14 10:26:56 +0800

    配置微信

---
+++ application/index/controller/common.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zavier
+ * Date: 2017/7/13
+ * Time: 上午11:13
+ */
+
+namespace app\index\controller;
+use Hooklife\ThinkphpWechat\Wechat;
+Wechat::user_tag()->lists();
+Wechat::card()->lists();
+
+class common
+{
+
+}
